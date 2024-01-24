e82c4f7b555ab19e9f5bf6881759984eab7f3527
Susanne Hamborg <shl@clin.au.dk>
2024-01-24 12:14:33 +0100

    Added more code along

--- R/learning.R
+++ R/learning.R
@@ -64,20 +64,23 @@ NHANES_phys <- NHANES_small %>%
   rename(physically_active = phys_active)
 
 
-# EXERCISE ----------------------------------------------------------------
+# Exercise ----------------------------------------------------------------
 
 
 nhanes_small <- NHANES_small
 
+# 1
 nhanes_small %>%
   select(bp_sys_ave, education)
 
+# 2
 nhanes_small %>%
   rename(
     bp_sys = bp_sys_ave,
     bp_dia = bp_dia_ave
   )
 
+# 3
 # rewrite this:
 select(nhanes_small, bmi, contains("age"))
 
@@ -86,6 +89,7 @@ nhanes_small %>%
   select(bmi, contains("age"))
 
 
+# 4
 # rewrite this:
 blood_pressure <- select(nhanes_small, starts_with("bp_"))
 rename(blood_pressure, bp_systolic = bp_sys_ave)
@@ -96,3 +100,81 @@ nhanes_small %>%
   rename(bp_systolic = bp_sys_ave)
 
 
+# Filtering data by row ---------------------------------------------------
+
+# The filter() function takes a logic condition (TRUE or FALSE).
+# As with the other functions, the first argument is the dataset and all others
+# are the logical conditions that will apply to the row filtering.
+
+filter(nhanes_small, phys_active == "No")
+# OR
+nhanes_small %>%
+  filter(phys_active == "No")
+
+# Participants who are physically active
+nhanes_small %>%
+  filter(phys_active != "No") %>%
+  select(phys_active)
+
+nhanes_small %>%
+  filter(bmi == 25) %>%
+  select(id, bmi)
+
+nhanes_small %>%
+  filter(bmi >= 25) %>%
+  select(id, bmi)
+
+# AND-operator: when you want to combine different values in a row (across columns)
+# NB! or &, both sides must be TRUE in order for the combination to be TRUE.
+#     For |, only one side needs to be TRUE in order for the combination to be TRUE.
+#  BE CAREFUL WITH THIS, ESPECIALLY OR !
+#  (if you only write a comma, then R sees it as AND)
+
+TRUE & TRUE
+TRUE & FALSE
+FALSE & FALSE
+TRUE | TRUE
+TRUE | FALSE
+FALSE | FALSE
+
+nhanes_small %>%
+  filter(bmi == 25 & phys_active == "No") %>%
+  select(bmi, phys_active)
+
+nhanes_small %>%
+  filter(bmi == 25 | phys_active == "No") %>%
+  select(bmi, phys_active)
+
+
+# Arranging rows ----------------------------------------------------------
+
+nhanes_small %>% # only for numerical and factor columns, not characters
+  arrange(age)
+
+nhanes_small %>%
+  arrange(education) %>%
+  select(education)
+
+nhanes_small %>%
+  arrange(desc(age)) # NB desc is also a function
+
+# arrange by several columns
+nhanes_small %>%
+  arrange(age, education) %>%
+  select(age, education)
+
+
+# Transform or add columns ------------------------------------------------
+
+nhanes_small %>%
+  mutate(
+    age = age * 12, # change age-column
+    log_bmi = log(bmi)
+  ) %>% # add new column, because the name is not in the dataset
+  select(id, age, bmi, log_bmi)
+
+nhanes_small %>%
+  mutate(
+    old = if_else(age >= 30, "Yes", "No")
+  ) %>%
+  select(age, old)
